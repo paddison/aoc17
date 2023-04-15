01eb31e3a138d7a37c3d956df067583d4092daf6
paddison <patrickbaumann579@gmail.com>
2023-04-15 20:52:17 +0200

    complete d21

--- aoc17/21/main.py
+++ aoc17/21/main.py
@@ -1,7 +1,4 @@
-import math
-
-
-START = '.#./..#/###'
+from math import sqrt
 
 
 def parse_input(filename) -> dict[str, str]:
@@ -9,34 +6,52 @@ def parse_input(filename) -> dict[str, str]:
         return {line.split('=>')[0].strip(): line.split('=>')[1].strip() for line in file}
 
 
-def str_to_list(pattern: str) -> list[list[bool]]:
-    return [[c == '#' for c in p] for p in pattern.split('/')]
+def str_to_list(pattern: str) -> list[str]:
+    return pattern.split('/')
 
 
-def list_to_str(pattern: list[list[bool]]):
-    return '/'.join(''.join('#' if x else '.' for x in row) for row in pattern)
+def list_to_str(pattern: list[str]):
+    return '/'.join(row for row in pattern)
 
 
-def rotate(pattern: list[list[bool]]) -> list[list[bool]]:
-    assert(4 > len(pattern) > 1)
+def rotate(pattern: list[str]) -> list[str]:
+    return list(map(lambda chars: ''.join(chars), zip(*pattern[::-1])))
+
+
+def flip(pattern: list[str]) -> list[str]:
+    return pattern[::-1]
+
+
+def split(pattern: list[str]) -> list[list[str]]:
     length = len(pattern)
-    return [[pattern[r][c] for r in reversed(range(length))] for c in range(length)]
+    divisor = 2 if length % 2 == 0 else 3
+    grid = list()
+    for i in range(0, length, divisor):
+        for j in range(0, length, divisor):
+            sublist = list()
+            for k in range(i, i + divisor):
+                sublist.append(pattern[k][j:j + divisor])
+            grid.append(sublist)
+    return grid
 
 
-def flip(pattern: list[list[bool]]) -> list[list[bool]]:
-    return [list(reversed(row)) for row in pattern]
+def join(patterns: list[list[str]]) -> list[str]:
+    total_length = len(patterns)
+    divisor = len(patterns[0])
+    length = int(sqrt(divisor * divisor * len(patterns)))
+    grid = list()
+    for i in range(0, total_length, length // divisor):
+        s = ''
+        for j in range(0, divisor):
+            s = ''
+            for k in range(i, i + length // divisor):
+                s += patterns[k][j]
+            grid.append(s)
+    return grid
 
 
-def split(pattern: list[list[bool]]) -> list[list[list[bool]]]:
-    splits = list()
-    for r in [0, 2]:
-        for c in [0, 2]:
-            splits.append([p[c:c + 2] for p in pattern[r:r + 2]])
-    return splits
-
-
-def apply_rule(pattern: list[list[bool]], rules: dict[str, str]) -> list[list[bool]]:
-    assert(4 > len(pattern) > 1)
+def apply_rule(pattern: list[str], rules: dict[str, str]) -> list[str]:
+    assert (4 > len(pattern) > 1)
     for _ in range(2):
         for _ in range(4):
             pattern_str = list_to_str(pattern)
@@ -46,24 +61,28 @@ def apply_rule(pattern: list[list[bool]], rules: dict[str, str]) -> list[list[bo
         pattern = flip(pattern)
 
 
-def print_grid(grid: list[list[list[bool]]]):
-    length = int(math.sqrt(len(grid)))
-    
+def iterate(n: int, pattern: list[str], rules: dict[str, str]) -> list[str]:
+    for n in range(n):
+        patterns = split(pattern)
+        new_patterns = list()
+        for pattern in patterns:
+            new_patterns.append(apply_rule(pattern, rules))
+        pattern = join(new_patterns)
+    return pattern
+
+
+def count_on(pattern: list[str]) -> int:
+    return sum(1 for row in pattern for p in row if p == "#")
+
 
 def main():
+    start = '.#./..#/###'
     rules = parse_input('data.txt')
-    grid = [str_to_list(START)]
-    for _ in range(5):
-        new_grid = list()
-        for pattern in grid:
-            new_pattern = apply_rule(pattern, rules)
-            if len(new_pattern) == 4:
-                new_grid.extend(split(new_pattern))
-            else:
-                new_grid.append(new_pattern)
-        grid = new_grid  # verify this works
-
-    print(sum(1 for p in grid for r in p for pixel in r if pixel))
+    pattern = str_to_list(start)
+    pattern = iterate(5, pattern, rules)
+    print(f'Part 1: {count_on(pattern)}')
+    pattern = iterate(13, pattern, rules)
+    print(f'Part 2: {count_on(pattern)}')
 
 
 if __name__ == '__main__':
